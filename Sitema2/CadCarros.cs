04baecb4aa3c69286165f00ee58055c683579800
walmir.asribeiro <walmir.asribeiro@AME0635270W11-1>
2025-02-04 12:03:29 -0300

    Adicionar arquivos de projeto.

---
+++ Sitema2/CadCarros.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sitema2
+{
+    public partial class CadCarros : Form
+    {
+        public CadCarros()
+        {
+            InitializeComponent();
+        }
+        private bool ValidarCpf(string cpf)
+        {
+            // Remove qualquer caractere não numérico
+            cpf = Regex.Replace(cpf, @"[^\d]", "");
+
+            // Verifica se tem 11 dígitos
+            if (cpf.Length != 11)
+                return false;
+
+            // Verifica se o CPF é uma sequência de números iguais (ex.: 111.111.111-11)
+            if (new string(cpf[0], 11) == cpf)
+                return false;
+
+            // Calculando o primeiro dígito verificador
+            int soma = 0;
+            int peso = 10;
+            for (int i = 0; i < 9; i++)
+            {
+                soma += int.Parse(cpf[i].ToString()) * peso--;
+            }
+
+            int resto = soma % 11;
+            int digito1 = resto < 2 ? 0 : 11 - resto;
+            if (digito1 != int.Parse(cpf[9].ToString()))
+                return false;
+
+            // Calculando o segundo dígito verificador
+            soma = 0;
+            peso = 11;
+            for (int i = 0; i < 10; i++)
+            {
+                soma += int.Parse(cpf[i].ToString()) * peso--;
+            }
+
+            resto = soma % 11;
+            int digito2 = resto < 2 ? 0 : 11 - resto;
+            return digito2 == int.Parse(cpf[10].ToString());
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBoxProprietario.Text = "";
+            maskedTextBoxCPF.Text = "";
+            maskedTextBoxTelefone.Text = "";
+            textBoxPlaca.Text = "";
+            maskedTextBoxAno.Text = "";
+            maskedTextBoxChassi.Text = "";
+            textBoxCor.Text = "";
+            textBoxModelo.Text = "";
+            textBoxMarca.Text = "";
+            maskedTextBoxValor.Text = "";
+            richTextBoxAcessorio.Text = "";
+            textBoxProprietario.Focus();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string cpf = maskedTextBoxCPF.Text;
+
+            if (ValidarCpf(cpf))
+            {
+                labelAlert.Text = "CPF VÁLIDO";
+                labelAlert.ForeColor = Color.Green;
+            }
+            else
+            {
+
+                labelAlert.Text = "CPF INVÁLIDO";
+                labelAlert.ForeColor = Color.Red;
+                maskedTextBoxCPF.Text = "";
+                maskedTextBoxCPF.Focus();
+            }
+        }
+
+        private void CadCarros_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
