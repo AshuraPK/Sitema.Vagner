272b740b2b816bcc7615d6693dfef9e9c6606541
Walmir Sousa <walmir.sousaa@gmail.com>
2025-02-11 09:53:13 -0300

    Update bd_sistema (2).sql

--- bd_sistema (2).sql
+++ bd_sistema (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 10/02/2025 às 16:03
+-- Tempo de geração: 11/02/2025 às 13:51
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.0.30
 
@@ -20,6 +20,82 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `bd_sistema`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `tb_carros`
+--
+
+CREATE TABLE `tb_carros` (
+  `Id_Carros` int(11) NOT NULL,
+  `Proprietario` varchar(150) NOT NULL,
+  `Telefone` varchar(10) NOT NULL,
+  `Cpf` varchar(14) NOT NULL,
+  `Placa` varchar(7) NOT NULL,
+  `Modelo` varchar(150) NOT NULL,
+  `Ano` year(4) NOT NULL,
+  `Chassi` varchar(17) NOT NULL,
+  `Cor` varchar(150) NOT NULL,
+  `Marca` varchar(150) NOT NULL,
+  `Acessorios` text NOT NULL,
+  `Valor` varchar(14) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `tb_clientes`
+--
+
+CREATE TABLE `tb_clientes` (
+  `Id_Cliente` int(11) NOT NULL,
+  `NomeCompleto` varchar(150) NOT NULL,
+  `Cpf` varchar(14) NOT NULL,
+  `Email` varchar(100) NOT NULL,
+  `Cep` varchar(9) NOT NULL,
+  `Numero` varchar(5) NOT NULL,
+  `Telefone` varchar(14) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `tb_clientes`
+--
+
+INSERT INTO `tb_clientes` (`Id_Cliente`, `NomeCompleto`, `Cpf`, `Email`, `Cep`, `Numero`, `Telefone`) VALUES
+(3, 'sdfsdf', '279.151.838-00', '645', '556  -', ' 6546', '(65) 5656 -');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `tb_carros`
+--
+ALTER TABLE `tb_carros`
+  ADD PRIMARY KEY (`Id_Carros`);
+
+--
+-- Índices de tabela `tb_clientes`
+--
+ALTER TABLE `tb_clientes`
+  ADD PRIMARY KEY (`Id_Cliente`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `tb_carros`
+--
+ALTER TABLE `tb_carros`
+  MODIFY `Id_Carros` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de tabela `tb_clientes`
+--
+ALTER TABLE `tb_clientes`
+  MODIFY `Id_Cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
